4a33eb1e2a2abf6e81c2634ea48313d31e9c2c5f
tudorfiiiase <floreatudor23@stud.ase.ro>
2025-01-03 21:21:24 +0200

    Add files via upload

---
+++ Student (set).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+
+public:
+    Student(const string& n, int a) : name(n), age(a) {}
+
+    string getName() const {
+        return name;
+    }
+
+    // Metodă setAge
+    void setAge(int newAge) {
+        if (newAge > 0) {
+            age = newAge;
+        }
+        else {
+            cout << "Varsta trebuie sa fie pozitiva!" << endl;
+        }
+    }
+
+    int getAge() const {
+        return age;
+    }
+};
