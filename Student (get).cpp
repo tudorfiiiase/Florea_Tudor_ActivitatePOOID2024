9a0d0ee5721fb970f70a2e0c9b8c99ca0c35a650
tudorfiiiase <floreatudor23@stud.ase.ro>
2025-01-03 21:17:13 +0200

    Add files via upload

---
+++ Student (get).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+
+public:
+    Student(const string& n) : name(n) {}
+    // Metodă getName
+    string getName() const {
+        return name;
+    }
+};
