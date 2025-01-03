d1eb7544e1940d1dc0e35325194565a69ca427a9
tudorfiiiase <floreatudor23@stud.ase.ro>
2025-01-03 21:04:58 +0200

    Add files via upload

---
+++ Laptop (friend).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Laptop {
+private:
+    std::string brand;
+    int ram;
+
+public:
+    Laptop(const std::string& brandName, int ramSize) : brand(brandName), ram(ramSize) {}
+
+    // Declarăm funcția friend
+    friend void getDetails(const Laptop& laptop);
+};
+
+// Funcția friend care accesează atributele private ale clasei Laptop
+void getDetails(const Laptop& laptop) {
+    cout << "Brand: " << laptop.brand << ", RAM: " << laptop.ram << " GB" << endl;
+}
+
+int main() {
+    Laptop laptop1("Dell", 16);  
+    getDetails(laptop1);        
+    Laptop laptop2("Apple", 32);
+    getDetails(laptop2);        // Apelăm funcția friend pentru un alt laptop
+    return 0;
+}
